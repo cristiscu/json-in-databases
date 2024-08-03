75578324056c9720145eced58fe434db4eb6b349
cristiscu <cristiscu@gmail.com>
2024-08-03 16:45:13 -0700

    update

--- sections/20-json-in-sqlite/01-sqlite3.py
+++ sections/20-json-in-sqlite/01-sqlite3.py
@@ -8,15 +8,16 @@ import pandas as pd
 # create SQLite database file 'sqlite.db', if not in the data/ folder,
 # and upload employees.csv file into a new 'employees' table
 db_filename = '../../data/sqlite.db'
-conn = sqlite3.connect(db_filename)
 if not os.path.exists(db_filename):
     df = pd.read_csv('../../data/employees.csv')
+    conn = sqlite3.connect(db_filename)
     df.to_sql(name='employees', con=conn)
 
 # query all 'employees' table by default
 # and dump content on screen
 sql = "select * from employees"
 query = st.text_area("SQL Query", value=sql)
+conn = sqlite3.connect(db_filename)
 cursor = conn.execute(query)
 
 res = pd.DataFrame.from_records(
